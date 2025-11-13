8912e32639c3cfc053343940e1e2f657a8d370d7
Antwaun Washington <antwaunwashington@gmail.comm>
2025-11-12 21:55:48 -0600

    recipe page and grid layout complete

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,11 +1,22 @@
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 import {useState} from 'react';
+import '../styles/recipeCard.scss';
 
 function RecipeCard(props) {
+    //Star rating code
+    const [recipeRating, setRecipeRating] = useState(0);
+
+    //state value for flipping the card back and forth
+    const [cardFlipped, setCardFlipped] = useState(false);
+
+    const flipCard = () => {
+        setCardFlipped(!cardFlipped);
+    }
 
     const [favorite, setFavorite] = useState(false);
 
@@ -17,16 +28,41 @@ function RecipeCard(props) {
     return (
         <div>
             <div className="Card-container">
-                <div className="Card-image">
-                    <img src={props.recipe.image} alt="Card-Image" width="200" height="150" />
-                </div>
-                <div className="Card-info">
-                    <h3 className="card-title" >{props.recipe.title}</h3>
-                        <h4 className="Difficulty">{props.recipe.difficulty}</h4>
-                            {/* code for the favorite heart to go from false to true */}
-                            <FontAwesomeIcon icon={favorite ? faHeart : faHeartRegular} onClick={ () => setFavorite(!favorite)} />
-                    
-                </div>
+                {cardFlipped ? (
+                    <>
+                        <div className="backCard">
+                            <div onClick={flipCard} >
+                                <div className="card-back-ingredients" >{props.recipe.ingredients}</div>
+                                    <h2 className="cook-time">Cook Time</h2>
+                                {/*the code below maps over the star array to repeat the image for a five star rating system on the back of the card */}
+                                    {/* Code below now allows a user to click on a star and allow for the stars before it to be highlighted as well */}
+                                    {[1,2,3,4,5].map(star =>
+                                        <FontAwesomeIcon className="star-ratings" key={star} icon={star <= recipeRating ? faStar : faStarRegular}onClick={(event) => {
+                                                                                                                                event.stopPropagation()
+                                                                                                                                setRecipeRating(star)}} />
+                                )}
+                            </div>
+                                <button className="slc-recipe-btn">
+                                     Select Recipe
+                                 </button>
+                                
+                        </div>    
+                    </>
+                ) : (   
+                <>
+                    <div className="frontCard">
+                        <div className="Card-image">
+                            <img src={props.recipe.image} alt="Card-Image" width="150" height="100" onClick={flipCard} />
+                        </div>
+                        <div className="Card-info">
+                            <h4 className="card-title" >{props.recipe.title}</h4>
+                                <h5 className="Difficulty">{props.recipe.difficulty}</h5>
+                                    {/* code for the favorite heart to go from false to true */}
+                                    <FontAwesomeIcon icon={favorite ? faHeart : faHeartRegular} onClick={ () => setFavorite(!favorite)} />
+                        </div>
+                    </div>             
+                </>
+                )} 
             </div>
         </div>
     )
--- src/pages/RecipeList.jsx
+++ src/pages/RecipeList.jsx
@@ -1,19 +1,111 @@
 import RecipeCard from '../components/RecipeCard';
 import Navbar from '../components/UpperNavBar';
+import {useState} from 'react'
 
 function GeneratedRecipes() {
 
-     const mockRecipe = {
+     const mockRecipe = [
+     {
+        id: 1,
         title: "Delicious Burger",
         image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400",
         difficulty: "Medium",
-        favorited: false
-    };
+        favorited: false,
+        ingredients: "Beef patty, lettuce, tomato, cheese, bun"
+    },
+    {
+        id: 2,
+        title: "Pasta Carbonara",
+        image: "https://images.unsplash.com/photo-1612874742237-6526221588e3?w=400",
+        difficulty: "Easy",
+        favorited: false,
+        ingredients: "Pasta, eggs, bacon, parmesan cheese"
+    },
+    {
+        id: 3,
+        title: "Pasta Carbonara",
+        image: "https://images.unsplash.com/photo-1612874742237-6526221588e3?w=400",
+        difficulty: "Easy",
+        favorited: false,
+        ingredients: "Pasta, eggs, bacon, parmesan cheese"
+    },
+    {
+        id: 4,
+        title: "Pasta Carbonara",
+        image: "https://images.unsplash.com/photo-1612874742237-6526221588e3?w=400",
+        difficulty: "Easy",
+        favorited: false,
+        ingredients: "Pasta, eggs, bacon, parmesan cheese"
+    },
+    {
+        id: 5,
+        title: "Pasta Carbonara",
+        image: "https://images.unsplash.com/photo-1612874742237-6526221588e3?w=400",
+        difficulty: "Easy",
+        favorited: false,
+        ingredients: "Pasta, eggs, bacon, parmesan cheese"
+    },
+    {
+        id: 6,
+        title: "Pasta Carbonara",
+        image: "https://images.unsplash.com/photo-1612874742237-6526221588e3?w=400",
+        difficulty: "Easy",
+        favorited: false,
+        ingredients: "Pasta, eggs, bacon, parmesan cheese"
+    },
+
+     ];
+
+     // ---- Pagination setup ----
+     const [currentPage, setCurrentPage] = useState(1);
+     const recipesPerPage = 6; // --- Can adjust based on screen size ---
+
+     const indexOfLastRecipe = currentPage * recipesPerPage;
+     const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+     const currentRecipes = mockRecipe.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+
+     const totalPages = Math.ceil(mockRecipe.length / recipesPerPage);
+
+     const nextPage = () => {
+        if (currentPage > totalPages) setCurrentPage(currentPage +1);
+     };
+
+     const prevPage = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1);
+     };
 
     return(
         <div>
             <Navbar />
-            <RecipeCard recipe={mockRecipe} />
+
+            {/*Recipe Grid */}
+            <div className="recipe-grid" >
+                {currentRecipes.map((recipe) => (
+                    <RecipeCard recipe={recipe} key={recipe.id} />
+                ))}
+                {/*Pagination Controls */}
+                <div className="pagination-controls">
+                    <button onClick={prevPage} disabled={currentPage === 1}>
+                        Previous
+                    </button>
+                    <span>
+                        Page {currentPage} of {totalPages}
+                    </span>
+                    <button onClick={nextPage} disabled={currentPage == totalPages}>
+                        Next
+                    </button>
+                </div>
+            </div>
+
+            {/*The code below takes the new array of recipe cards, loops through them using .map and creates a card for each individual recipe */}
+            <div className="recipe-grid" >
+                {mockRecipe.map(recipe => (
+                    <RecipeCard recipe={recipe} key={recipe.id} />            
+                ))}
+            </div>
+
+            
         </div>
     )
 }
